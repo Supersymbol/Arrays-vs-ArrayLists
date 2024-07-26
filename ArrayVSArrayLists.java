689ea487a16c25f4ad224e68038623acf6d19e75
Dieu-aime <133424225+Supersymbol@users.noreply.github.com>
2024-07-25 22:28:24 -0400

    Update ArrayVSArrayLists.java

--- ArrayVSArrayLists.java
+++ ArrayVSArrayLists.java
@@ -51,8 +51,6 @@ public class ArrayVSArrayLists {
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.print(arrayList.get(i) + " ");
         }
-        System.out.println();
-        
-        // Size flexibility
+        System.out.println();        
     }
 }
